d93d5bebc2eaca602a1c56ea1c84f5eddccdff2f
CleberSGuedes <guedes.unemat@gmail.com>
2025-06-17 10:25:18 -0400

    pta.versão 1 - subação/entregas - cadastrar

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
-# config.py
+# pta/config.py
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        'mssql+pyodbc://nger:S3duc2025@10.112.2.56:31433/spo'
-        '?driver=ODBC+Driver+18+for+SQL+Server'
-        '&Encrypt=no'  # ou 'yes' se exigido pelo servidor
+        f"mssql+pyodbc://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+        f"?driver={os.getenv('DB_DRIVER')}&Encrypt={os.getenv('DB_ENCRYPT')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'pta2026_super_seguro'
+    SECRET_KEY = os.getenv('SECRET_KEY')
